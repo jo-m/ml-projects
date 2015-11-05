5d1363beb13def0fecc4f9068d319f8c5297d55e
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-05 16:39:01 +0100

    svc grid search, no results

--- project2/process.py
+++ project2/process.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import sklearn.cross_validation as skcv
 import sklearn.metrics as skmet
 
@@ -79,7 +79,20 @@ def run_validate(Xtrain, Ytrain, model):
 
 def run_gridsearch(X, Y, model):
     parameters = {
-        'reg__n_estimators': [100, 150, 200, 250, 500, 1000]
+    # C=1.0, kernel='rbf', degree=3, gamma=0.0, coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200
+        # 'reg__n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 50],
+        # 'reg__penalty': ['l1', 'l2'],
+        # 'reg__alpha': [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1],
+        # 'reg__epsilon': [0.01, 0.02, 0.05, 0.1, 0.2, 0.5],
+        # 'reg__C': [0.5, .6, .7, .8, 1, 1.5, 2, 2.5, 3],
+        # 'reg__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+        # 'reg__degree': [2, 3, 4, 5],
+        # 'reg__gamma': [0.0, 0.1, 0.01, 0.001, 0.0001, 0.2, 0.02, 0.002, 0.0002, 0.3, 0.03, 0.003, 0.0003, 0.05, 0.15, 0.08, 0.13],
+        'reg__gamma': np.linspace(0.05, 0.15),
+        # 'reg__probability': [True, False],
+        # 'reg__shrinking': [True, False],
+        # 'reg__multi_class': ['ovr', 'multinomial'],
+        # 'reg__loss' : ['deviance', 'exponential'],
     }
 
     grid = GridSearchCV(model, parameters, verbose=1, n_jobs=-1)
@@ -91,7 +104,7 @@ def run_gridsearch(X, Y, model):
 
 def build_pipe():
     scaler = StandardScaler()
-    regressor = RandomForestClassifier(n_estimators=10)
+    regressor = SVC(kernel='rbf', probability=True, shrinking=True)
     return Pipeline([
         ('scaler', scaler),
         ('reg', regressor),
@@ -99,7 +112,7 @@ def build_pipe():
 
 Xtrain, Ytrain = load_data()
 pipe = build_pipe()
-# pipe = run_gridsearch(Xtrain, Ytrain, pipe)
+pipe = run_gridsearch(Xtrain, Ytrain, pipe)
 run_crossval(Xtrain, Ytrain, pipe)
 run_split(Xtrain, Ytrain, pipe)
 run_validate(Xtrain, Ytrain, pipe)
